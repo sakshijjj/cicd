2434c66b8beaacd5212215420f9d073f4e29a276
sakshijjj <92265898+sakshijjj@users.noreply.github.com>
2023-07-04 00:16:23 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,4 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-   public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
 }
